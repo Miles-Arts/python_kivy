46428f216c1ff234fa7604db3f3b46e474bef32e
Milton <gratno@gmail.com>
2025-06-05 14:29:28 -0500

    Firts Hello world KIVY APP

---
+++ hello_world.py
@@ -0,0 +1,18 @@
+#hello_world.py
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+class MainApp(App):
+    def build(self):
+        label = Label(text='VendeFacil App')        
+        return label
+    
+if __name__ == '__main__':
+    app = MainApp()
+    app.run()
+    
+    
+    
+
+
